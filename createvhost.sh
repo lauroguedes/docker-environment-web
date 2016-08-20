860f3f11a38390d56b007473d827cfab9e46b764
Léo <lauro.guedes@eagletecnologia.com>
2016-08-20 08:24:47 -0300

    Criação do executável para gerar os virtual hosts

---
+++ createvhost.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Informe o nome do servidor (Ex.: siteexemplo) :"
+read vhost
+ 
+echo "Informe o caminho do site (Ex.: pastapai/pastafilha) :"
+read path
+ 
+echo "Criando configuração de VHost para o servidor"
+
+docker exec -d web sudo sh /serve.sh $vhost $path
+
+echo "127.0.0.1		$vhost" >> "/etc/hosts"
+
+echo "Vhost criado!"
