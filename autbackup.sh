0c29c32b8f4cd21a827a5f70754ce8b53fe419d9
Léo <lauro.guedes@eagletecnologia.com>
2016-12-19 15:28:40 -0200

    Retirada do comando sudo do arquivo autbackup.sh

--- autbackup.sh
+++ autbackup.sh
@@ -8,7 +8,7 @@ echo " " >> $LOG
 echo "|-----------------------------------------------" >> $LOG
 echo " Sincronização iniciada em $INICIO" >> $LOG
 
-sudo rsync -Cravzp ~/apps/ /mnt/externalhd/backup_webserver >> $LOG
+rsync -Cravzp --progress ~/apps/ /mnt/externalhd/backup_webserver >> $LOG
 FINAL=`date +%d/%m/%Y-%H:%M:%S`
 
 echo " Sincronização Finalizada em $FINAL" >> $LOG
