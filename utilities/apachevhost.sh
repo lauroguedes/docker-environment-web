19d6e795eaa8a32027e597bc054b5dad818b4a8f
lauroguedes <leo-ti@hotmail.com>
2017-04-18 09:47:19 -0300

    Configuração de apelido do sistema

--- utilities/apachevhost.sh
+++ utilities/apachevhost.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Informe o nome do servidor (Ex.: siteexemplo) :"
-read vhost
-
-echo "Informe o caminho do site (Ex.: pastapai/pastafilha) :"
-read path
-
 echo "Criando configuração de VHost para o servidor"
 
-docker exec -d apache-php-7.1 sh /server.sh $vhost $path
+docker exec -d apache-php-7.1 sh /server.sh $1 $2
 
-echo "127.0.0.1         $vhost" >> "/etc/hosts"
+echo "127.0.0.1         $1" >> "/etc/hosts"
+
+if [ -e $3 ];
+then
+  echo "Host without alias"
+else
+  if [ $3 = "--alias" ];
+  then
+    echo "alias c-a7-$1='docker run -it --rm -v $(pwd):/apps/www/$2 leowgweb/apache-php-7.1 composer'" >> "/etc/bash.bashrc"
+    echo "alias n-a7-$1='docker run -it --rm -v $(pwd):/apps/www/$2 leowgweb/apache-php-7.1 npm'" >> "/etc/bash.bashrc"
+    echo "alias g-a7-$1='docker run -it --rm -v $(pwd):/apps/www/$2 leowgweb/apache-php-7.1 gulp'" >> "/etc/bash.bashrc"
+  fi
+fi
 
 echo "Vhost criado!"
