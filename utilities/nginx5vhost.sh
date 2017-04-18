e55e43c83a390c36b4dc594fa0ba9debe46fdbfd
lauroguedes <leo-ti@hotmail.com>
2017-04-18 09:43:33 -0300

    Configuração de apelidos para o sistema

--- utilities/nginx5vhost.sh
+++ utilities/nginx5vhost.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-
-echo "Informe o nome do servidor (Ex.: siteexemplo) :"
-read vhost
- 
-echo "Informe o caminho do site (Ex.: pastapai/pastafilha) :"
-read path
  
 echo "Criando configuração de VHost para o servidor"
 
-docker exec -d nginx-php-5.6 sh /serve.sh $vhost $path
+docker exec -d nginx-php-5.6 sh /serve.sh $1 $2
 
-echo "127.0.0.1		$vhost" >> "/etc/hosts"
+echo "127.0.0.1		$1" >> "/etc/hosts"
+
+if [ -e $3 ];
+then
+  echo "Host without alias"
+else
+  if [ $3 = "--alias" ];
+  then
+    echo "alias c-n5-$1='docker run -it --rm -v $(pwd):/apps/www/$2 shincoder/homestead:php5.6 composer'" >> "/etc/bash.bashrc"
+    echo "alias n-n5-$1='docker run -it --rm -v $(pwd):/apps/www/$2 shincoder/homestead:php5.6 npm'" >> "/etc/bash.bashrc"
+    echo "alias g-n5-$1='docker run -it --rm -v $(pwd):/apps/www/$2 shincoder/homestead:php5.6 gulp'" >> "/etc/bash.bashrc"
+  fi
+fi
 
 echo "Vhost criado!"
