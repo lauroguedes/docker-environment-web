29ca7e900a011a1e0665d34487de0e834b262c25
lauroguedes <leo-ti@hotmail.com>
2017-04-18 09:40:46 -0300

    Criação da configuração para apelidos
    Se colocado o parâmetro --aliases grava os apelidos do composer, npm e gulp no sistema

--- utilities/nginx7vhost.sh
+++ utilities/nginx7vhost.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Informe o nome do servidor (Ex.: siteexemplo) :"
-read vhost
- 
-echo "Informe o caminho do site (Ex.: pastapai/pastafilha) :"
-read path
- 
 echo "Criando configuração de VHost para o servidor"
 
-docker exec -d nginx-php-7.0 sudo sh /serve.sh $vhost $path
+docker exec -d nginx-php-7.0 sudo sh /serve.sh $1 $2
 
-echo "127.0.0.1		$vhost" >> "/etc/hosts"
+echo "127.0.0.1		$1" >> "/etc/hosts"
+
+if [ -e $3 ];
+then
+  echo "Host without alias"
+else
+  if [ $3 = "--alias" ];
+  then
+    echo "alias c-n7-$1='docker run -it --rm -v $(pwd):/apps/www/$2 shincoder/homestead:php7.0 composer'" >> "/etc/bash.bashrc"
+    echo "alias n-n7-$1='docker run -it --rm -v $(pwd):/apps/www/$2 shincoder/homestead:php7.0 npm'" >> "/etc/bash.bashrc"
+    echo "alias g-n7-$1='docker run -it --rm -v $(pwd):/apps/www/$2 shincoder/homestead:php7.0 gulp'" >> "/etc/bash.bashrc"
+  fi
+fi
 
 echo "Vhost criado!"
